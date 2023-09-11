96602085c352469dcafa1714aab4046753b1c754
Pedro Freitas <pevictor.freitas@gmail.com>
2023-09-11 16:14:31 +0200

    Connect to database
    Connect to mongoDB atlas.

--- src/Utils/router.js
+++ src/Utils/router.js
@@ -2,6 +2,17 @@ const { Router } = require("express");
 
 const router = Router();
 
+// TO DO - ROUTERS
+// ADD USER
+// LOGIN
+// LOGOUT
+// VIEW ALL PHOTOS
+// LIKE PHOTO
+// POST PHOTO
+// DELETE PHOTO
+// EDIT PHOTO
+// CHECK USER PROFILE
+
 router.get("/", (req, res) => {
   return res.send("TESTE");
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = require("./Utils/router");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongodbUri = process.env.MONGODB_URI;
 
 const app = express();
 
+mongoose
+  .connect(mongodbUri)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.use(express.json());
-app.use(router)
-
-
+app.use(router);
 
 app.listen(3333, () => console.log("Server running on port 3333"));
