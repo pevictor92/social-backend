4dd09f698a446db9f809de693b23896cdb72b0dc
Pedro Freitas <pevictor.freitas@gmail.com>
2023-09-24 19:19:47 +0200

    Fix cors error
    Fix cors error.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = require("./Utils/router");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 require("dotenv").config();
 
 const mongodbUri = process.env.MONGODB_URI;
@@ -19,6 +20,11 @@ mongoose
 
 app.use(express.json());
 app.use(router);
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.listen(process.env.PORT, () => console.log("Server running"));
